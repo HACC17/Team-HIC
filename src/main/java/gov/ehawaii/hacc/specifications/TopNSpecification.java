323b5c66e319864dd318a78dc93a504d62477201
BJ Peter DeLaCruz <bjpeter@ehawaii.gov>
2017-09-08 13:26:04 -1000

    Added comments.

--- src/main/java/gov/ehawaii/hacc/specifications/TopNSpecification.java
+++ src/main/java/gov/ehawaii/hacc/specifications/TopNSpecification.java
@@ -5,6 +5,12 @@ import org.apache.logging.log4j.Logger;
 import gov.ehawaii.hacc.repositories.impl.SqlStatements;
 import lombok.Getter;
 
+/**
+ * This specification is used to retrieve the top N data.
+ * 
+ * @author BJ Peter DeLaCruz <bjpeter@ehawaii.gov>
+ * @version 1.0
+ */
 @Getter
 public class TopNSpecification implements SqlSpecification {
 
@@ -14,6 +20,13 @@ public class TopNSpecification implements SqlSpecification {
   private final String column1;
   private final String column2;
 
+  /**
+   * Creates a new TopNSpecification.
+   * 
+   * @param n N, a number greater than 0.
+   * @param column1 The column that will be specified in the <code>GROUP BY</code> clause.
+   * @param column2 The column whose values are summed together.
+   */
   public TopNSpecification(int n, String column1, String column2) {
     this.n = n;
     this.column1 = column1;
@@ -35,6 +48,11 @@ public class TopNSpecification implements SqlSpecification {
     throw new UnsupportedOperationException("This method is not supported.");
   }
 
+  /**
+   * Returns the following query:<br /><br />&nbsp;&nbsp;<code>SELECT [column1], SUM([column2]) FROM GRANTS</code>.
+   * 
+   * @return A <code>SELECT</code> statement without any other clauses, e.g. <code>WHERE</code>, <code>GROUP BY</code>, etc.
+   */
   public String getSelect() {
     return String.format(SqlStatements.GET_TOP_N_DATA, column1, column2);
   }
