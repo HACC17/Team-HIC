d26bab332ca94f6da0524960de4f71096c2af8d2
BJ Peter DeLaCruz <bjpeter@ehawaii.gov>
2017-09-07 15:22:32 -1000

    Added comments.

--- src/main/java/gov/ehawaii/hacc/specifications/Specification.java
+++ src/main/java/gov/ehawaii/hacc/specifications/Specification.java
@@ -1,5 +1,11 @@
 package gov.ehawaii.hacc.specifications;
 
+/**
+ * Implementations of this interface contain one or more criteria used to filter results from a database query, in a list, etc.
+ * 
+ * @author BJ Peter DeLaCruz <bjpeter@ehawaii.gov>
+ * @version 1.0
+ */
 public interface Specification {
 
 }
--- src/main/java/gov/ehawaii/hacc/specifications/SqlSpecification.java
+++ src/main/java/gov/ehawaii/hacc/specifications/SqlSpecification.java
@@ -1,13 +1,39 @@
 package gov.ehawaii.hacc.specifications;
 
+/**
+ * Implementations of this interface contain criteria used to filter results from a SQL query (MySQL, Oracle, SQL Server, etc.).
+ * 
+ * @author BJ Peter DeLaCruz <bjpeter@ehawaii.gov>
+ * @version 1.0
+ */
 public interface SqlSpecification extends Specification {
 
+  /**
+   * The name of the table used in the query.
+   * 
+   * @return The name of the table.
+   */
   String getTable();
 
+  /**
+   * The name of the column used in the query.
+   * 
+   * @return The name of the column.
+   */
   String getColumn();
 
+  /**
+   * The value used to filter results from a SQL query.
+   * 
+   * @return The value.
+   */
   Object getValue();
 
+  /**
+   * The query that will be used to get results from a SQL database.
+   * 
+   * @return The SQL query.
+   */
   String toSqlClause();
 
 }
