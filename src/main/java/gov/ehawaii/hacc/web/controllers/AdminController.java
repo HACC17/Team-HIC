e6a1d36c1427dd563a3c02bea0f5560ff2b302f8
BJ Peter DeLaCruz <bjpeter@ehawaii.gov>
2017-09-12 15:01:56 -1000

    Added another modal.

--- src/main/java/gov/ehawaii/hacc/web/controllers/AdminController.java
+++ src/main/java/gov/ehawaii/hacc/web/controllers/AdminController.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,64 +37,64 @@ public class AdminController {
   @Autowired
   private ExcelImporter excelImporter;
 
-  /**
-   * Displays the main page for an admin user. The page contains a form that the admin submits in
-   * order to add a grant to the database.
-   * 
-   * @param model The model to which to add data for the form.
-   * @return The name of the JSP file that contains the form.
-   */
-  @RequestMapping(method = RequestMethod.GET)
-  public final String showAdminPage(final Model model) {
-    model.addAttribute("grant", new Grant());
-    model.addAttribute("organizations", grantsService.getAllOrganizations());
-    model.addAttribute("statuses", grantsService.getAllGrantStatuses());
-    model.addAttribute("priorities", grantsService.getAllStrategicPriorities());
-    model.addAttribute("results", grantsService.getAllStrategicResults());
-    model.addAttribute("types", grantsService.getAllGrantTypes());
-    model.addAttribute("locations", grantsService.getAllLocations());
-    return "admin/index";
-  }
 
   /**
-   * When a <code>GET</code> request is sent to the <code>/import</code> endpoint, the application
-   * will import sample data from a file. The type of the file depends on the value of the parameter
-   * that is sent with the request. Two file types are supported:
+   * When a <code>GET</code> request is sent to the <code>/admin/import</code> endpoint, the application will import
+   * sample data from a file. The type of the file depends on the value of the parameter that is sent with the request.
+   * Two file types are supported:
    * <ul>
    * <li><code>csv</code>: A CSV file</li>
    * <li><code>excel</code>: A Microsoft Excel file</li>
    * </ul>
    * 
-   * @param ext The type of the file from which to get data to import.
+   * @param type The type of the file from which to get data to import.
+   * @param response The server response.
    * @return The admin user will be redirected to the <code>/admin</code> endpoint.
+   * @throws IOException If there are problems trying to send the response back to the client.
    */
   @RequestMapping(value = "/import", method = RequestMethod.GET)
-  public final String importSampleData(@RequestParam("ext") final String ext) {
-    if (ext == null || ext.isEmpty()) {
-      LOGGER.error("ext parameter is null or empty.");
-      return "redirect:/admin";
-    }
+  public final void importSampleData(@RequestParam("type") final String type, HttpServletResponse response)
+      throws IOException {
+    Assert.notNull(type, "type must not be null");
 
-    switch (ext) {
+    String msg;
+    switch (type) {
     case "csv":
       if (csvImporter.importData()) {
-        LOGGER.info("Successfully imported sample data from CSV file.");
+        msg = "Successfully imported sample data from a CSV file.";
+        LOGGER.info(msg);
+        response.setStatus(HttpServletResponse.SC_OK);
+      }
+      else {
+        msg = "There was a problem trying to import sample data from a CSV file.";
+        LOGGER.error(msg);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
       }
       break;
     case "excel":
       if (excelImporter.importData()) {
-        LOGGER.info("Successfully imported sample data from Excel file.");
+        msg = "Successfully imported sample data from a Microsoft Excel file.";
+        LOGGER.info(msg);
+        response.setStatus(HttpServletResponse.SC_OK);
+      }
+      else {
+        msg = "There was a problem trying to import sample data from a Microsoft Excel file.";
+        LOGGER.error(msg);
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
       }
       break;
     default:
-      LOGGER.error(ext + " not supported, yet.");
+      msg = type + " is not yet supported.";
+      LOGGER.error(type + " is not yet supported.");
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
-    return "redirect:/admin";
+    response.getWriter().write(msg);
   }
 
+
   /**
-   * A <code>POST</code> request is sent to the <code>/admin</code> endpoint to add a grant to the
-   * database. Information about the grant is sent along with the request.
+   * A <code>POST</code> request is sent to the <code>/admin</code> endpoint to add a grant to the database. Information
+   * about the grant is sent along with the request.
    * 
    * @param grant The grant to add to the database.
    * @return The admin user will be redirected to the <code>/admin</code> endpoint.
@@ -110,6 +110,14 @@ public class AdminController {
     return "redirect:/";
   }
 
+
+  /**
+   * A <code>GET</code> request is sent to the <code>/admin/opendata</code> endpoint to push all the data stored in the
+   * repository to Hawaii's Open Data Portal.
+   * 
+   * @param response The server response.
+   * @throws IOException If there are problems trying to send the response back to the client.
+   */
   @RequestMapping(value = "/opendata", method = RequestMethod.GET)
   public final void pushToOpenData(HttpServletResponse response) throws IOException {
     response.getWriter().write("Test");
