025f957218379097285d4175a12fa67a3130271b
BJ Peter DeLaCruz <bjpeter@ehawaii.gov>
2017-09-15 20:49:56 -1000

    Added scripts.

---
+++ scripts/load_preference.sh
@@ -0,0 +1,1 @@
+java -jar $PATH_TO_LIB/DataSync-1.7.2.jar -t LoadPreferences -c $PATH_TO_CONFIG_FILE/config.txt
---
+++ scripts/run.sh
@@ -0,0 +1,1 @@
+java -jar $PATH_TO_LIB/DataSync-1.7.2.jar $PATH_TO_SIJ_FILE/ohagrants.sij
